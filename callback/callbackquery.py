3f8b2ab209f03ed5263039b20eed0aa55998549e
emin <m6mintm@gmail.com>
2024-01-30 17:22:28 +0300

    Begin to work

--- callback/callbackquery.py
+++ callback/callbackquery.py
@@ -1,12 +1,10 @@
-from aiogram import types, Router, F
-
+from aiogram import types, Router, F, Bot
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Update
 
 from keyboards.inline import sale_menu
 
-router_query = Router()
 
-
-@router_query.callback_query(F.data.startswitch('buy'))
-async def buy(callback: types.CallbackQuery):
+async def buy_handler(call: CallbackQuery, bot: Bot):
     pic = 'AgACAgEAAxkBAAMrZa_prvjsFGrgkm9ArzDReGUdL5MAAhCsMRtuFoFFHavg8sGnsF8BAAMCAAN4AAM0BA'
-    await callback.message.answear(pic, caption='Привет! Выбери товар..', reply_markup=sale_menu)
+    await call.message.answear(pic, caption='Привет! Выбери товар..', reply_markup=sale_menu)
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
+import os
 import asyncio
 from aiogram import Bot, Dispatcher, F
-from configure.config import TOKEN
 import logging
 from commands.commands import router_commands, start
 from filters.filters import router_photo_filter
-from callback.callbackquery import router_query
-from callback.callbackquery import buy
+from commands.bot_commands import bot_commands
+from callback.callbackquery import buy_handler
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
     dp.include_router(router_commands)
     dp.include_router(router_photo_filter)
-
-    dp.include_router(router_query)
-    dp.callback_query
+    dp.message.register(bot_commands, commands = 'admin')
+    dp.callback_query.register(buy_handler, F.data.startswith('buy'))
 
     try:
-        await dp.start_polling(bot)
+        await dp.start_polling(bot, allowed_updates=True)
     finally:
         await bot.close()
 
