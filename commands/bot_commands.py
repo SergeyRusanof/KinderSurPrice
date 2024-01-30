189553abe71a3637387039e5d1cb75214351e9c1
emin <m6mintm@gmail.com>
2024-01-30 17:55:32 +0300

    Begin to work

--- commands/bot_commands.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot
-from aiogram.types import BotCommand, BotCommandScope, Update
-
-
-async def bot_commands(bot: Bot):
-    commands = [
-        BotCommand(command='start', description='Начало'),
-        BotCommand(command='admin', description='Для админа', scope=BotCommandScope.SUPERUSER)
-    ]
